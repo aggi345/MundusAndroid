cf51dc52a0a52f06cab0d7b21c37961477649236
aggi345 <agnar-97@hotmail.com>
2020-02-18 14:31:02 +0000

    Update

--- app/src/androidTest/java/is/hi/HBV601G/mundusandroid/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/is/hi/HBV601G/mundusandroid/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package is.hi.HBV601G.mundusandroid;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
