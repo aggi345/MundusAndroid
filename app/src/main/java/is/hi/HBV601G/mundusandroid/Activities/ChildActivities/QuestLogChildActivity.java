ca51f61160354c049fa84ea1bc9d2c245ff66991
zeppelin-43 <dantgud@gmail.com>
2020-02-24 15:12:42 +0000

    Added activities

---
+++ app/src/main/java/is/hi/HBV601G/mundusandroid/Activities/ChildActivities/QuestLogChildActivity.java
@@ -0,0 +1,16 @@
+package is.hi.HBV601G.mundusandroid.Activities.ChildActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import is.hi.HBV601G.mundusandroid.R;
+
+public class QuestLogChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest_log_child);
+    }
+}
---
+++ app/src/main/java/is/hi/HBV601G/mundusandroid/Activities/ParentActivities/QuestLogParentActivity.java
@@ -0,0 +1,16 @@
+package is.hi.HBV601G.mundusandroid.Activities.ParentActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import is.hi.HBV601G.mundusandroid.R;
+
+public class QuestLogParentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest_log_parent);
+    }
+}
